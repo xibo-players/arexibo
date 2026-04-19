1b6c6fbd7772e16e78e5fd0170d659a88c0f1dda
Georg Brandl <georg@python.org>
2026-04-19 22:48:25 +0200

    Reimplement zeromq socket for XMR

--- src/xmr.rs
+++ src/xmr.rs
@@ -3,13 +3,16 @@
 
 //! Receive, decrypt and handle incoming XMR messages from CMS.
 
-use anyhow::{Context, Result};
+use anyhow::{bail, Context, Result};
 use base64::{Engine, engine::general_purpose::STANDARD as BASE64};
+use byteorder::{BE, ReadBytesExt};
 use crossbeam_channel::{Receiver, Sender, unbounded};
 use rsa::RsaPrivateKey;
 use serde::{Deserialize, Deserializer, de::Error};
 use serde_json::from_slice;
 use time::{OffsetDateTime, Duration};
+use std::net::TcpStream;
+use std::io::{Read, Write};
 use crate::config::CmsSettings;
 
 /// Possible messages to forward to the collect thread.
@@ -25,9 +28,7 @@ pub enum Message {
 pub struct Manager {
     private_key: RsaPrivateKey,
     sender: Sender<Message>,
-    #[allow(unused)]  // need to hold onto the context
-    context: zmq::Context,
-    socket: zmq::Socket,
+    socket: ZmqSubSocket,
 }
 
 const HEARTBEAT: &[u8] = b"H";
@@ -36,18 +37,14 @@ impl Manager {
     pub fn new(settings: &CmsSettings, connect: &str,
                private_key: RsaPrivateKey) -> Result<(Self, Receiver<Message>)> {
         let channel = settings.xmr_channel();
-        let context = zmq::Context::new();
-        let socket = context.socket(zmq::SUB).context("creating XMR socket")?;
-        socket.connect(connect).context("connecting XMR socket")?;
-        socket.set_linger(0)?;
-        socket.set_subscribe(channel.as_bytes())?;
-        socket.set_subscribe(HEARTBEAT)?;
+        let mut socket = ZmqSubSocket::connect(connect).context("connecting XMR socket")?;
+        socket.subscribe(channel.as_bytes())?;
+        socket.subscribe(HEARTBEAT)?;
         let (sender, receiver) = unbounded();
 
         Ok((Self {
             private_key,
             sender,
-            context,
             socket,
         }, receiver))
     }
@@ -61,12 +58,12 @@ impl Manager {
     }
 
     fn process_msg(&mut self) -> Result<()> {
-        let channel = self.socket.recv_msg(0)?;
-        assert!(channel.get_more());
-        let key = self.socket.recv_msg(0)?;
-        assert!(key.get_more());
-        let content = self.socket.recv_msg(0)?;
-        assert!(!content.get_more());
+        let (channel, more) = self.socket.recv_frame()?;
+        assert!(more);
+        let (key, more) = self.socket.recv_frame()?;
+        assert!(more);
+        let (content, more) = self.socket.recv_frame()?;
+        assert!(!more);
         if &*channel != HEARTBEAT {
             let json_msg = JsonMessage::new(&self.private_key, &key, &content)?;
             log::debug!("got XMR message: {:?}", json_msg);
@@ -137,6 +134,93 @@ fn decrypt_private_key(enc_key: &[u8], private_key: &RsaPrivateKey) -> Result<Ve
     let dec_data = private_key.decrypt(rsa::Pkcs1v15Encrypt, enc_key).context("failed to decrypt PK")?;
     Ok(dec_data)
 }
+
+struct ZmqSubSocket(TcpStream);
+
+/// Implementation of ZMTP as far as we need it for XMR. We don't want to pull in the
+/// `zmq` crate since it is almost unmaintained.
+impl ZmqSubSocket {
+    fn connect(uri: &str) -> Result<Self> {
+        let rx = regex::Regex::new("tcp://([^:]*):([0-9]+)").context("invalid validation Regex")?;
+        let caps = rx.captures(uri).context("invalid XMR connect URI")?;
+        let host = caps.get(1).expect("present").as_str();
+        let port = caps[2].parse().expect("digits");
+
+        let mut stream = TcpStream::connect((host, port))?;
+        stream.set_read_timeout(Some(std::time::Duration::from_secs(1)))?;
+
+        // greeting: signature, version (3.0), security (none) and server flag (no),
+        // then pad to 64 bytes
+        stream.write_all(b"\xff\x00\x00\x00\x00\x00\x00\x00\x01\x7f\
+                           \x03\x00\
+                           NULL\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\
+                           \x00\
+                           \x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\
+                           \x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00")?;
+        // read greeting from peer
+        let mut buf = [0; 64];
+        stream.read_exact(&mut buf)?;
+        if buf[0] != 0xff || buf[9] != 0x7f || buf[10] != 0x03 || &buf[12..16] != b"NULL" {
+            bail!("ZMTP greeting not understood");
+        }
+
+        // send ready command
+        stream.write_all(b"\x04\x19\x05READY\x0bSocket-Type\x00\x00\x00\x03SUB")?;
+        // read ready command
+        stream.read_exact(&mut buf[..2])?;
+        if buf[0] != 0x04 {
+            bail!("ZMTP command frame not understood");
+        }
+        let len = buf[1] as usize;
+        if len >= 62 {
+            bail!("ZMTP command frame too long");
+        }
+        stream.read_exact(&mut buf[2..2+len])?;
+        if &buf[2..8] != b"\x05READY" {
+            bail!("ZMTP READY command not understood");
+        }
+
+        // now we're ready to receive frames
+        stream.set_read_timeout(None)?;
+        Ok(Self(stream))
+    }
+
+    fn subscribe(&mut self, topic: &[u8]) -> Result<()> {
+        let mut msg = Vec::with_capacity(3 + topic.len());
+        msg.push(0);  // single-frame message, short length
+        msg.push(1 + topic.len() as u8);  // length of msg
+        msg.push(1);  // subscribe command
+        msg.extend_from_slice(topic);
+        self.0.write_all(&msg)?;
+        Ok(())
+    }
+
+    fn recv_frame(&mut self) -> Result<(Vec<u8>, bool)> {
+        let flags = self.0.read_u8()?;
+        let more = flags & 1 != 0;
+        let long_len = flags & 2 != 0;
+        let len = if long_len {
+            self.0.read_u64::<BE>()? as usize
+        } else {
+            self.0.read_u8()? as usize
+        };
+        let mut result = vec![0; len];
+        self.0.read_exact(&mut result)?;
+        Ok((result, more))
+    }
+}
+
+// #[test]
+// fn test_zmq() {
+//     let mut socket = ZmqSubSocket::connect("tcp://localhost:5555").unwrap();
+//     socket.subscribe(b"test").unwrap();
+//     let first = socket.recv_frame().unwrap();
+//     let second = socket.recv_frame().unwrap();
+//     assert!(first.1);
+//     assert!(!second.1);
+//     assert_eq!(&*first.0, b"test");
+//     assert_eq!(&*second.0, b"content");
+// }
 
 #[test]
 fn test_decrypt() {
